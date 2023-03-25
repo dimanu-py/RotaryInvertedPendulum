3f7b05e3ac6ed2c6fbabd139d5f74abaed17c375
Diego Martínez <diegomtz126@gmail.com>
2023-03-25 20:48:21 +0100

    Convert DataInterface into a real interface and implement specific classes for reading and inserting data

--- source/data_interface.py
+++ source/data_interface.py
@@ -3,11 +3,45 @@ import pandas as pd
 from source.database_controller import DatabaseController
 from source.matlab_files_controller import MatlabFilesController
 
+from abc import ABC, abstractmethod
 
-class DataInterface:
+
+class DataInterface(ABC):
     def __init__(self):
         self.matlab_files_controller = MatlabFilesController()
         self.database_controller = DatabaseController(create_db_connection=True)
+
+    @abstractmethod
+    def read_matlab_file(self) -> pd.DataFrame:
+        pass
+
+    @abstractmethod
+    def insert_data_to_database(self, data: pd.DataFrame, table_name: str) -> None:
+        pass
+
+    @abstractmethod
+    def read_data_from_database(self, table_name: str) -> pd.DataFrame:
+        pass
+
+
+class DataReader(DataInterface):
+    def __init__(self):
+        super().__init__()
+
+    def read_matlab_file(self) -> pd.DataFrame:
+        raise NotImplementedError('DataReader can only access database.')
+
+    def insert_data_to_database(self, data: pd.DataFrame, table_name: str) -> None:
+        raise NotImplementedError('DataReader must read data from database not insert it.')
+
+    def read_data_from_database(self, table_name: str) -> pd.DataFrame:
+        data = self.database_controller.read_data(table_name=table_name)
+        return data
+
+
+class DataInserter(DataInterface):
+    def __init__(self):
+        super().__init__()
 
     def read_matlab_file(self) -> pd.DataFrame:
         dataframe = self.matlab_files_controller.get_signals_data()
@@ -18,5 +52,4 @@ class DataInterface:
                                              data=data)
 
     def read_data_from_database(self, table_name: str) -> pd.DataFrame:
-        data = self.database_controller.read_data(table_name=table_name)
-        return data
+        raise NotImplementedError('DataInserter can not read data from database, only inserte it.')
