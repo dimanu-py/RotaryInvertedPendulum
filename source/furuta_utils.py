f2d40b7dc106cdd59d60300a31aa1291dc5690bb
Diego Martínez <diegomtz126@gmail.com>
2023-03-25 23:17:40 +0100

    Add save_file_as_parquet and read_parquet in furuta_utils.py

--- source/furuta_utils.py
+++ source/furuta_utils.py
@@ -1,4 +1,8 @@
+import os
+import pyarrow
+import pandas as pd
 import yaml
+import pyarrow.parquet as pq
 
 
 def read_yaml_parameters(yaml_path=None) -> dict:
@@ -15,3 +19,22 @@ def read_yaml_parameters(yaml_path=None) -> dict:
 
         except Exception as e:
             print(e)
+
+
+def save_file_as_parquet(data: pd.DataFrame, save_path: str, file_name: str) -> None:
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    if isinstance(data, pd.DataFrame):
+        data.to_parquet(path=f"{save_path}/{file_name}.parquet",
+                        index=False)
+
+    else:
+        with open(f"{save_path}/{file_name}.parquet", 'wb') as file:
+            table = pyarrow.Table.from_pandas(df=data)
+            pq.write_table(table=table,
+                           where=file)
+
+
+def load_parquet(path, file):
+    return pd.read_parquet(path=f"{path}/{file}.pkl")
