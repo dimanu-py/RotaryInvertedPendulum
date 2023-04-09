ef0aa2730b6ec8e917e20f55646fbb2a42ffc0b7
Diego Martínez <diegomtz126@gmail.com>
2023-04-10 01:17:12 +0200

    Update helpers runner

--- source/helpers_data_management.py
+++ source/helpers_data_management.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
+from source.env_settings import EnvSettings
 from source.helpers.matlab_files_controller import MatlabFilesController
 from source.helpers.data_saver import SaveFile
 
 
+# TODO: acceder a las variables de entorno para los paths
 class HelpersRun:
 
     def __init__(self, matlab_controller: MatlabFilesController, file_saver: SaveFile):
@@ -20,3 +22,14 @@ class HelpersRun:
 
     def save_data(self, dataframe: pd.DataFrame) -> None:
         self.file_saver.save_file(dataframe=dataframe)
+
+
+if __name__ == '__main__':
+    env_vars = EnvSettings.get_settings()
+
+    matlab = MatlabFilesController(matlab_folder=env_vars.MATLAB_PATH)
+    dataset_saver = SaveFile(folder_path=env_vars.DATASETS_PATH)
+
+    helper_runner = HelpersRun(matlab_controller=matlab,
+                               file_saver=dataset_saver)
+    helper_runner.run()
