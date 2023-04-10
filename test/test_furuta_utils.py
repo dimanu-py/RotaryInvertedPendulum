bd8205305ec3d58fbb021ea4d90913953079fb6f
Diego Martínez <diegomtz126@gmail.com>
2023-04-10 22:40:37 +0200

    Update test for furuta_utils

--- test/test_furuta_utils.py
+++ test/test_furuta_utils.py
@@ -1,11 +1,19 @@
+import os.path
+
 import pytest
 
-from source.furuta_utils import read_yaml_parameters
+from source.helpers.furuta_utils import read_yaml_parameters
 
 
-@pytest.mark.parametrize('yaml_path', [r'C:\PROGRAMACION\PENDULO INVERTIDO\Pendulo Invertido Diego\Python-Furuta-Pendulum\test\yaml_test.yaml'])
-def test_read_yaml_parameters(yaml_path):
-    parameters = read_yaml_parameters(yaml_path=yaml_path)
+@pytest.fixture
+def default_yaml_path():
+    current_file_path = os.path.dirname(__file__)
+    return os.path.join(current_file_path, 'yaml_test.yaml')
 
+
+def test_read_yaml_parameters(default_yaml_path):
+    parameters = read_yaml_parameters(yaml_path=default_yaml_path)
+
+    assert parameters is not None
     assert isinstance(parameters, dict)
 
