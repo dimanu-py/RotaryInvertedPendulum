4bd835539c7a941d84499aedf4999c3c19e82d52
Diego Martínez <diegomtz126@gmail.com>
2023-04-16 01:14:39 +0200

    Solve TODO (don't access config in data_saver.py helper) and add docstrings

--- source/helpers/data_saver.py
+++ source/helpers/data_saver.py
@@ -2,36 +2,39 @@ import os
 
 import pandas as pd
 
-from source.helpers.furuta_utils import read_yaml_parameters
-
 
 class SaveFile:
+    """
+    Class to save data into a file.
+    The class decides which file format to use based on the extension.
+    """
     def __init__(self, folder_path: str) -> None:
         self.save_folder_path = folder_path
-
-        configuration_params = read_yaml_parameters().get('data_saver')
-        self.file_name = configuration_params['file_name']
-        self.extension = configuration_params['extension'].lower()
 
         self.save_funcs = {'parquet': SaveParquet,
                            'pickle': SavePickle}
 
-    def save_file(self, dataframe: pd.DataFrame) -> None:
+    def save_file(self, dataframe: pd.DataFrame, extension: str, file_name: str) -> None:
         """
         Save a dataframe with a specified file format.
         :param dataframe: dataframe to save
+        :param extension: file format to save
+        :param file_name: name of the file to be saved
         """
-        selected_saver = self.save_funcs.get(self.extension)
+        selected_saver = self.save_funcs.get(extension)
 
         if not selected_saver:
-            raise ValueError(f"Invalid file format {self.extension}")
+            raise ValueError(f"Invalid file format {extension}")
 
         saver_instance = selected_saver(self.save_folder_path)
         saver_instance.save_file(dataframe=dataframe,
-                                 save_file_name=self.file_name)
+                                 save_file_name=file_name)
 
 
 class SaveParquet:
+    """
+    Class to save data into a parquet file.
+    """
     def __init__(self, folder_path: str) -> None:
         self.folder_path = folder_path
 
@@ -55,6 +58,9 @@ class SaveParquet:
 
 
 class SavePickle:
+    """
+    Class to save data into a pickle file.
+    """
     def __init__(self, folder_path: str) -> None:
         self.folder_path = folder_path
 
