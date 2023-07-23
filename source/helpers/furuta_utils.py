a1a6db9161377b70dd84202e35308bcc88205b8d
Diego Martínez <diegomtz126@gmail.com>
2023-07-23 20:32:21 +0200

    Refactor of furuta_utils.py and tests

--- source/helpers/furuta_utils.py
+++ source/helpers/furuta_utils.py
@@ -1,33 +1,23 @@
-import os
-
 import yaml
 
 
-def read_yaml_parameters(yaml_path: str = None) -> dict:
+def read_yaml_parameters(yaml_path: str) -> dict:
     """
     Read yaml file and load content in a dictionary.
-    :param yaml_path: path to yaml file
-    :return: content as a dictionary
     """
-    if yaml_path is None:  # default path
-        yaml_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../config/parameters.yaml'))
-
-    with open(yaml_path, 'r') as params_file:
-        try:
+    try:
+        with open(yaml_path, 'r') as params_file:
             parameters = yaml.load(stream=params_file,
                                    Loader=yaml.SafeLoader)
 
             return parameters
-
-        except Exception as e:
-            print(f'Error loading yaml {e.args[1]}')
+    except FileNotFoundError as error:
+        print(f'Impossible to find the yaml file -> {error.args[1]}')
 
 
 def extract_extension(file_name: str) -> str:
     """
     Extract file extension from a file name.
-    :param file_name: name of the file
-    :return: file extension
     """
     extension = file_name.split('.')[-1]
     return extension
--- test/test_furuta_utils.py
+++ test/test_furuta_utils.py
@@ -3,17 +3,34 @@ import os.path
 import pytest
 
 from source.helpers.furuta_utils import read_yaml_parameters
+from source.helpers.furuta_utils import extract_extension
 
 
 @pytest.fixture
-def default_yaml_path():
+def yaml_testing():
+    """
+    Fixture to configure the path for the yaml used it to tests
+    """
     current_file_path = os.path.dirname(__file__)
     return os.path.join(current_file_path, 'yaml_test.yaml')
 
 
-def test_read_yaml_parameters(default_yaml_path):
-    parameters = read_yaml_parameters(yaml_path=default_yaml_path)
+def test_read_yaml_parameters(yaml_testing):
+    """
+    Tests that a yaml is read correctly
+    """
+    parameters = read_yaml_parameters(yaml_path=yaml_testing)
 
     assert parameters is not None
     assert isinstance(parameters, dict)
 
+
+def test_extract_extension(yaml_testing):
+    """
+    Tests that extension is extracted correctly from any file
+    """
+    extension = extract_extension(file_name=yaml_testing)
+
+    assert extension is not None
+    assert isinstance(extension, str)
+    assert extension == 'yaml'
