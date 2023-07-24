a3b0ca978e2bb45ffcb0046b1de1069c14e4a566
Diego Martínez <diegomtz126@gmail.com>
2023-07-24 20:55:11 +0200

    Refactor matlab_files_controller.py and modify tests with mocked data

--- source/helpers/matlab_files_controller.py
+++ source/helpers/matlab_files_controller.py
@@ -3,52 +3,42 @@ import os
 import hdf5storage
 import pandas as pd
 
-from source.helpers.furuta_utils import read_yaml_parameters
-
 
 class MatlabFilesController:
     """
     Class to read data from a matlab file.
     The location of the file is outside the project folder.
     """
-    def __init__(self, matlab_folder: str):
+    def __init__(self, matlab_folder: str, config_yaml_path: str) -> None:
         self.matlab_folder_path = matlab_folder
+        self.config_yaml_path = config_yaml_path
 
-        configuration_params = read_yaml_parameters().get('matlab_controller')
-        self.mat_file_name = configuration_params['file_name']
-        self.mat_data_name = configuration_params['data_name']
-        self.columns_name = configuration_params['columns_name']
-
-    def transform_matlab_to_dataframe(self) -> pd.DataFrame:
+    def transform_matlab_to_dataframe(self, data_file_name: str, mat_data_name: str, columns_name: list[str]) -> pd.DataFrame:
         """
-        Organize and store data from matlab file as a dataframe.
-        :return: data as a dataframe
+        Stores data from matlab file as a dataframe.
         """
-        matlab_data = self.load_matlab_file(folder_path=self.matlab_folder_path, file_name=self.mat_file_name)
+        matlab_data = self._load_matlab_file(mat_file_name=data_file_name)
         try:
-            signals_data = matlab_data.get(self.mat_data_name).transpose()
+            signals_data = matlab_data.get(mat_data_name).transpose()
 
             df_signals = pd.DataFrame(data=signals_data,
-                                      columns=self.columns_name)
+                                      columns=columns_name)
 
             return df_signals
 
-        except Exception as e:
-            print(f'Error converting matlab data to dataframe {e.args[1]}')
+        except Exception as error:
+            print(f'Error converting matlab data to dataframe -> {error.args[1]}')
 
-    @staticmethod
-    def load_matlab_file(folder_path: str, file_name: str) -> dict:
+    def _load_matlab_file(self, mat_file_name: str) -> dict:
         """
         Read matlab file with array structure.
-        :param folder_path: folder where the matlab file is located
-        :param file_name: file name to read
-        :return: data file
         """
         try:
             # Matlab folder and file should always exist
-            matlab_file_path = os.path.join(folder_path, file_name)
+            matlab_file_path = os.path.join(self.matlab_folder_path,
+                                            mat_file_name)
             matlab_data = hdf5storage.loadmat(file_name=matlab_file_path)
             return matlab_data
 
         except Exception as e:
-            print(f'Error loading matlab file {e.args[1]}')
+            print(f'Error loading matlab file -> {e.args[1]}')
