673cb97ee49c2607e4c6d8ecc31c0a05c30d4715
Diego Martínez <diegomtz126@gmail.com>
2023-07-24 20:57:56 +0200

    Update test_furuta_utils.py with mocked data

--- test/test_furuta_utils.py
+++ test/test_furuta_utils.py
@@ -7,29 +7,28 @@ from source.helpers.furuta_utils import extract_extension
 
 
 @pytest.fixture
-def yaml_testing():
+def yaml_path():
     """
     Fixture to configure the path for the yaml used it to tests
     """
-    current_file_path = os.path.dirname(__file__)
-    return os.path.join(current_file_path, 'yaml_test.yaml')
+    return '../test/mock_data/read_parameter_yaml_test.yaml'
 
 
-def test_read_yaml_parameters(yaml_testing):
+def test_read_yaml_parameters(yaml_path):
     """
     Tests that a yaml is read correctly
     """
-    parameters = read_yaml_parameters(yaml_path=yaml_testing)
+    parameters = read_yaml_parameters(yaml_path=yaml_path)
 
     assert parameters is not None
     assert isinstance(parameters, dict)
 
 
-def test_extract_extension(yaml_testing):
+def test_extract_extension(yaml_path):
     """
     Tests that extension is extracted correctly from any file
     """
-    extension = extract_extension(file_name=yaml_testing)
+    extension = extract_extension(file_name=yaml_path)
 
     assert extension is not None
     assert isinstance(extension, str)
