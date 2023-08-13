7f4a603b34da91a79b70e719d76a5ec8367e88bd
dimanu <diego.martinez@frenetic.ai>
2023-08-13 14:57:42 +0200

    Refactor use of configuration object when creating dataset

--- source/furuta_pendulum.py
+++ source/furuta_pendulum.py
@@ -1,28 +1,29 @@
 from source.env_settings import EnvSettings
 from source.helpers.data_saver import SaveParquet
 from source.helpers.matlab_files_controller import MatlabFilesController
+from source.helpers.configuration_builder import Configuration, RawDatasetConfigurationBuilder
 from source.workers.dataset_creator import DatasetCreator
-from source.helpers.configuration_reader import RawDatasetConfiguration
 
 env_vars = EnvSettings.get_env_vars()
+
+MATLAB_PATH = env_vars.MATLAB_PATH
+DATASETS_PATH = env_vars.DATASETS_PATH
+PARAMETERS_PATH = env_vars.PARAMS_PATH
 
 
 class FurutaPendulum:
 
-    MATLAB_PATH = env_vars.MATLAB_PATH
-    DATASETS_PATH = env_vars.DATASETS_PATH
-    PARAMETERS_PATH = env_vars.PARAMS_PATH
-
     def __init__(self):
-        self.matlab_controller = MatlabFilesController(matlab_folder=self.MATLAB_PATH)
-        self.dataset_saver = SaveParquet(folder_path=self.DATASETS_PATH)
-        self.configuration = RawDatasetConfiguration(configuration_path=self.PARAMETERS_PATH)
+        self.matlab_controller = MatlabFilesController(matlab_folder=MATLAB_PATH)
+        self.dataset_saver = SaveParquet(folder_path=DATASETS_PATH)
 
     def create_dataset(self):
+        raw_dataset_configuration = Configuration(builder=RawDatasetConfigurationBuilder())
         dataset_creator = DatasetCreator(matlab_controller=self.matlab_controller,
-                                         data_saver=self.dataset_saver)
+                                         data_saver=self.dataset_saver,
+                                         configuration=raw_dataset_configuration)
 
-        dataset_creator.create_dataset(configuration=self.configuration)
+        dataset_creator.create_dataset(parameters_path=PARAMETERS_PATH)
 
 
 if __name__ == '__main__':
--- test/test_configuration_reader.py
+++ test/test_configuration_reader.py
@@ -1,9 +1,10 @@
-from source.helpers.configuration_reader import RawDatasetConfiguration, RawDatasetFactory
+from source.helpers.configuration_builder import Configuration, RawDatasetConfigurationBuilder
 from source.env_settings import EnvSettings
 
 env_vars = EnvSettings.get_env_vars()
 
 
 def test_create_neural_network():
-    configuration = RawDatasetConfiguration(config_factory=RawDatasetFactory())
-    configuration.create_configuration(configuration_path=env_vars.PARAMS_PATH)
+    configuration = Configuration(builder=RawDatasetConfigurationBuilder())
+    raw_dataset_configuration = configuration.construct(env_vars.PARAMS_PATH, 'raw_dataset')
+    assert raw_dataset_configuration.matlab_configuration is not None
