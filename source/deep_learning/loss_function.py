8e2c2e8ebce35a7f715dd474075db72f9f1619e1
dimanu <diego.martinez@frenetic.ai>
2023-08-15 19:10:48 +0200

    Create loss function encapsulation with its tests

---
+++ source/deep_learning/loss_function.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+from abc import abstractmethod, ABC
+
+from source.helpers.configuration_builder import Configuration
+
+
+class LossFunction:
+    """Class to encapsulate the loss function"""
+    def __init__(self, configuration: "Configuration") -> None:
+        self.configuration = configuration
+        self.loss_function = self.create_loss_function()
+
+    def create_loss_function(self) -> "LossFunction":
+        loss_function_type = self.configuration.loss_function_config.loss
+        loss_function_classes = {'mse': MeanSquaredError,
+                                 'mae': MeanAbsoluteError,
+                                 'msle': MeanSquaredLogarithmicError}
+
+        try:
+            loss_function = loss_function_classes[loss_function_type](configuration=self.configuration)
+            return loss_function
+        except KeyError:
+            print(f'Loss function {loss_function_type} not implemented yet.')
+
+    def get_loss_function(self) -> "LossFunction":
+        return self.loss_function
+
+
+class LossFunctionFactory(ABC):
+    """Abstract class to encapsulate the configuration of the loss function"""
+    def __init__(self, configuration: "Configuration") -> None:
+        self.configuration = configuration
+        self.loss_function = self.create_loss_function()
+
+    @abstractmethod
+    def create_loss_function(self):
+        pass
+
+
+class MeanSquaredError(LossFunctionFactory):
+    """Class to encapsulate the configuration of the mean squared error loss function"""
+    def create_loss_function(self):
+        return tf.keras.losses.MeanSquaredError()
+
+
+class MeanAbsoluteError(LossFunctionFactory):
+    """Class to encapsulate the configuration of the mean absolute error loss function"""
+    def create_loss_function(self):
+        return tf.keras.losses.MeanAbsoluteError()
+
+
+class MeanSquaredLogarithmicError(LossFunctionFactory):
+    """Class to encapsulate the configuration of the mean squared logarithmic error loss function"""
+    def create_loss_function(self):
+        return tf.keras.losses.MeanSquaredLogarithmicError()
