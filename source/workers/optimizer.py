6c980bfede230ba7fa16452581f664058863210c
dimanu <diego.martinez@frenetic.ai>
2023-08-20 20:35:08 +0200

    Refactor optimizer factory

--- source/workers/optimizer.py
+++ source/workers/optimizer.py
@@ -4,13 +4,12 @@ from abc import abstractmethod, ABC
 from source.helpers.configuration_builder import Configuration
 
 
-class Optimizer:
+class OptimizerFactory:
     """Class to encapsulate the optimizer"""
     def __init__(self, configuration: "Configuration") -> None:
         self.configuration = configuration
-        self.optimizer = self.create_optimizer()
 
-    def create_optimizer(self) -> "Optimizer":
+    def get_optimizer(self) -> "Optimizer":
         """Method to create the optimizer dynamically based on the configuration"""
         optimizer_type = self.configuration.optimizer_config.optimizer_type
         optimizer_classes = {'adam': AdamOptimizer,
@@ -23,39 +22,35 @@ class Optimizer:
         except KeyError:
             print(f'Optimizer {optimizer_type} not implemented yet.')
 
-    def get_optimizer(self) -> "Optimizer":
-        """Method to get the optimizer without accessing the optimizer attribute directly"""
-        return self.optimizer
-
 
 # TODO: try to create some logic to be able to create the optimizers with a variable number of parameters
-class OptimizerFactory(ABC):
+class Optimizer(ABC):
     """Abstract class to encapsulate the configuration of the optimizer"""
     def __init__(self, configuration: "Configuration") -> None:
         self.configuration = configuration
-        self.optimizer = self.create_optimizer()
+        self.optimizer = self.configure_optimizer()
 
     @abstractmethod
-    def create_optimizer(self):
+    def configure_optimizer(self):
         pass
 
 
 class AdamOptimizer(OptimizerFactory):
     """Class to encapsulate the configuration of the Adam optimizer"""
-    def create_optimizer(self):
-        learning_rate = self.configuration.optimizer_config.learning_rate
+    def configure_optimizer(self):
+        learning_rate = self.configuration.learning_rate
         return tf.keras.optimizers.Adam(learning_rate=learning_rate)
 
 
 class SGDOptimizer(OptimizerFactory):
     """Class to encapsulate the configuration of the SGD optimizer"""
-    def create_optimizer(self):
+    def configure_optimizer(self):
         learning_rate = self.configuration.optimizer_config.learning_rate
         return tf.keras.optimizers.SGD(learning_rate=learning_rate)
 
 
 class RMSPropOptimizer(OptimizerFactory):
     """Class to encapsulate the configuration of the RMSProp optimizer"""
-    def create_optimizer(self):
+    def configure_optimizer(self):
         learning_rate = self.configuration.optimizer_config.learning_rate
         return tf.keras.optimizers.RMSprop(learning_rate=learning_rate)
