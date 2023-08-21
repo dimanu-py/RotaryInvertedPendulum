160e333814e168a7b549bad5aaca16b951484350
dimanu <diego.martinez@frenetic.ai>
2023-08-21 19:14:59 +0200

    Refactor loss function factory approach

--- source/workers/loss_function.py
+++ source/workers/loss_function.py
@@ -1,57 +1,53 @@
-import tensorflow as tf
+from keras.losses import (MeanSquaredError,
+                          MeanAbsoluteError,
+                          MeanSquaredLogarithmicError)
 from abc import abstractmethod, ABC
 
 from source.helpers.configuration_builder import Configuration
 
 
-class LossFunction:
+class LossFunctionFactory:
     """Class to encapsulate the loss function"""
-    def __init__(self, configuration: "Configuration") -> None:
-        self.configuration = configuration
-        self.loss_function = self.create_loss_function()
-
-    def create_loss_function(self) -> "LossFunction":
+    @staticmethod
+    def get_loss_function(configuration) -> "LossFunction":
         """Method to create the loss function dynamically based on the configuration"""
-        loss_function_type = self.configuration.loss_function_config.loss
-        loss_function_classes = {'mse': MeanSquaredError,
-                                 'mae': MeanAbsoluteError,
-                                 'msle': MeanSquaredLogarithmicError}
+        loss_function_type, = configuration.loss
+        loss_function_classes = {'mse': MSE,
+                                 'mae': MAE,
+                                 'msle': MSLE}
 
         try:
-            loss_function = loss_function_classes[loss_function_type](configuration=self.configuration)
-            return loss_function
+            loss_function = loss_function_classes[loss_function_type](configuration=configuration)
+            return loss_function.loss_function
         except KeyError:
             print(f'Loss function {loss_function_type} not implemented yet.')
 
-    def get_loss_function(self) -> "LossFunction":
-        """Method to get the loss function without accessing the loss_function attribute directly"""
-        return self.loss_function
 
-
-class LossFunctionFactory(ABC):
+# TODO: try to create some logic to be able to create the loss function with a variable number of parameters
+class LossFunction(ABC):
     """Abstract class to encapsulate the configuration of the loss function"""
     def __init__(self, configuration: "Configuration") -> None:
         self.configuration = configuration
-        self.loss_function = self.create_loss_function()
+        self.loss_function = self.configure_loss_function()
 
     @abstractmethod
-    def create_loss_function(self):
+    def configure_loss_function(self):
         pass
 
 
-class MeanSquaredError(LossFunctionFactory):
+class MSE(LossFunction):
     """Class to encapsulate the configuration of the mean squared error loss function"""
-    def create_loss_function(self):
-        return tf.keras.losses.MeanSquaredError()
+    def configure_loss_function(self):
+        return MeanSquaredError()
 
 
-class MeanAbsoluteError(LossFunctionFactory):
+class MAE(LossFunction):
     """Class to encapsulate the configuration of the mean absolute error loss function"""
-    def create_loss_function(self):
-        return tf.keras.losses.MeanAbsoluteError()
+    def configure_loss_function(self):
+        return MeanAbsoluteError()
 
 
-class MeanSquaredLogarithmicError(LossFunctionFactory):
+class MSLE(LossFunction):
     """Class to encapsulate the configuration of the mean squared logarithmic error loss function"""
-    def create_loss_function(self):
-        return tf.keras.losses.MeanSquaredLogarithmicError()
+    def configure_loss_function(self):
+        return MeanSquaredLogarithmicError()
