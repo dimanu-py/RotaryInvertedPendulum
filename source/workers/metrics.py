ed21aea097dba91db2f726998a7857921e2b5457
dimanu <diego.martinez@frenetic.ai>
2023-08-21 19:15:24 +0200

    Refactor metrics factory approach

--- source/workers/metrics.py
+++ source/workers/metrics.py
@@ -1,79 +1,78 @@
-import tensorflow as tf
+from keras.metrics import (MeanAbsoluteError,
+                           MeanSquaredError,
+                           RootMeanSquaredError,
+                           R2Score,
+                           MeanSquaredLogarithmicError,
+                           MeanAbsolutePercentageError)
 from abc import abstractmethod, ABC
 from typing import List
 
 from source.helpers.configuration_builder import Configuration
 
 
-class Metrics:
+class MetricsFactory:
     """Class to encapsulate the metrics"""
-    def __init__(self, configuration: "Configuration") -> None:
-        self.configuration = configuration
-        self.metrics = self.create_metrics()
-
-    def create_metrics(self) -> List["Metrics"]:
+    @staticmethod
+    def get_metrics(configuration: "Configuration") -> List["Metrics"]:
         """Method to create the metrics dynamically based on the configuration"""
-        metrics_type = self.configuration.metrics_config.metrics
-        metrics_classes = {'mae': MeanAbsoluteError,
-                           'mse': MeanSquaredError,
-                           'rmse': RootMeanSquaredError,
-                           'mape': MeanAbsolutePercentageError,
-                           'msle': MeanSquaredLogarithmicError,
-                           'r2': R2Score}
+        metrics_type = configuration.metrics
+        metrics_classes = {'mae': MAE,
+                           'mse': MSE,
+                           'rmse': RMSE,
+                           'mape': MAPE,
+                           'msle': MSLE,
+                           'r2': R2,
+                           }
 
         try:
-            metrics = [metrics_classes[metric](self.configuration) for metric in metrics_type]
-            return metrics
+            metrics = [metrics_classes[metric](configuration) for metric in metrics_type]
+            return [metric.metrics for metric in metrics]
         except KeyError:
             print(f'Metrics {metrics_type} not implemented yet.')
 
-    def get_metrics(self):
-        """Method to get the metrics without accessing the metrics attribute directly"""
-        return self.metrics
 
-
-class MetricsFactory(ABC):
+class Metrics(ABC):
     """Abstract class to encapsulate the configuration of the metrics"""
     def __init__(self, configuration: "Configuration") -> None:
         self.configuration = configuration
-        self.metrics = self.create_metrics()
+        self.metrics = self.configure_metrics()
 
     @abstractmethod
-    def create_metrics(self):
+    def configure_metrics(self):
         pass
 
 
-class MeanAbsoluteError(MetricsFactory):
+class MAE(Metrics):
     """Class to encapsulate the configuration of the Mean Absolute Error metric"""
-    def create_metrics(self):
-        return tf.keras.metrics.MeanAbsoluteError()
+    def configure_metrics(self):
+        return MeanAbsoluteError()
 
 
-class MeanSquaredError(MetricsFactory):
+class MSE(Metrics):
     """Class to encapsulate the configuration of the Mean Squared Error metric"""
-    def create_metrics(self):
-        return tf.keras.metrics.MeanSquaredError()
+    def configure_metrics(self):
+        return MeanSquaredError()
 
 
-class RootMeanSquaredError(MetricsFactory):
+class RMSE(Metrics):
     """Class to encapsulate the configuration of the Root Mean Squared Error metric"""
-    def create_metrics(self):
-        return tf.keras.metrics.RootMeanSquaredError()
+    def configure_metrics(self):
+        return RootMeanSquaredError()
 
 
-class MeanAbsolutePercentageError(MetricsFactory):
+class MAPE(Metrics):
     """Class to encapsulate the configuration of the Mean Absolute Percentage Error metric"""
-    def create_metrics(self):
-        return tf.keras.metrics.MeanAbsolutePercentageError()
+    def configure_metrics(self):
+        return MeanAbsolutePercentageError()
 
 
-class MeanSquaredLogarithmicError(MetricsFactory):
+class MSLE(Metrics):
     """Class to encapsulate the configuration of the Mean Squared Logarithmic Error metric"""
-    def create_metrics(self):
-        return tf.keras.metrics.MeanSquaredLogarithmicError()
+    def configure_metrics(self):
+        return MeanSquaredLogarithmicError()
 
 
-class R2Score(MetricsFactory):
+class R2(Metrics):
     """Class to encapsulate the configuration of the R2 Score metric"""
-    def create_metrics(self):
-        return tf.keras.metrics.R2Score()
+    def configure_metrics(self):
+        return R2Score()
